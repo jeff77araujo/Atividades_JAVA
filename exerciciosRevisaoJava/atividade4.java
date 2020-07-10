940cc5ef5007bcecd47786bba6ef5473454a645d
Jeff Araujo <jefferson_lord@yahoo.com.br>
2020-07-10 15:26:05 -0300

    atualizando a resolução da atividade

--- exerciciosRevisaoJava/atividade4.java
+++ exerciciosRevisaoJava/atividade4.java
@@ -2,12 +2,14 @@ package exerciciosRevisãoJAVA;
 
 public class atividade4 {
 	public static void main(String args[]) {
-		float numero = 1, calculo;
+		float numero = 1, calculo, soma = 0;
 
 		for (int i = 1; i <= 50; i++) {
 			calculo = numero / i;
+			soma = soma + calculo;
 			System.out.println(numero + " / " + i + " = " + calculo);
 			numero += 2;
 		}
+		System.out.println("\nSOMA TOTAL: " + soma);
 	}
 }
